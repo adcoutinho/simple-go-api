ee948f3aaab85365048a2dd730671274b4965735
Adriano Coutinho <adriano.coutinho@pagar.me>
2022-06-20 18:02:14 -0300

    chore: listen to 0.0.0.0:8080

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -29,7 +29,7 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
-	router.Run("localhost:8080")
+	router.Run()
 }
 
 // getAlbums responds with the list of all albums as JSON
